66fba6dac4ab6e9e58aead45f985893e8b361178
kharu217 <artwins007@gmail.com>
2024-04-24 20:57:24 +0900

    개요 작성 중(배터리 부족)

--- TensorFlow_start.py
+++ TensorFlow_start.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import keras
 import tensorflow_datasets as tfds
 import numpy as np
 
@@ -9,3 +10,10 @@ dateset = tf.data.Dataset.from_tensor_slices(x)
 
 #2.텐서플로에서 제공하는 데이터셋 사용
 ds = tfds.load('mnist', split='train', shuffle_files=True)
+
+#3.케라스에서 제공하는 데이터셋 사용
+data_train, data_test = tf.keras.datasets.mnist.load_data()
+
+#4.인터넷에서 데이터셋을 로컬 컴퓨터에 내려받아 사용
+urls = '링크'
+text_path = tf.keras
